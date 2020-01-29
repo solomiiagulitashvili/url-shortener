3b3e7cf4ab6bf725a3c70ed08b7ebade4e661166
solomiia <solomiiagulitashvili@gmail.com>
2020-01-29 16:14:07 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const bodyParser = require('body-parser');
+const urlShortener = require('node-url-shortener');
+
+app.get('/',(req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
+
+app.post('/url', (req, res) => {
+  const url = req.body.url;
+  urlShortener.short(url, (err, shortenUrl) => {
+    res.send(shortenUrl);
+  });
+});
+ 
+
+app.listen(port, () => {
+  console.log(`url-shortenet listeming on port ${port}`);
+});
