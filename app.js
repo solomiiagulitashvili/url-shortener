c4aa572be47b1590bf9182c06fa94667bffb6fcc
solomiia <solomiiagulitashvili@gmail.com>
2020-01-29 18:48:05 +0200

    postgreSQL db added

--- app.js
+++ app.js
@@ -4,20 +4,36 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const urlShortener = require('node-url-shortener');
+const db = require('./models/index.js');
+const exphbs = require('express-handlebars');
 
-app.get('/',(req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+
+
+app.get('/', function(req, res) {
+  db.Url.findAll({order: [['createdAt', 'DESC']], limit: 5})
+  .then(urlObjs => {
+    res.render('index', {
+      urlObjs: urlObjs
+    });
+  });
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
 
-app.post('/url', (req, res) => {
-  const url = req.body.url;
-  urlShortener.short(url, (err, shortenUrl) => {
-    res.send(shortenUrl);
+app.post('/url', function(req, res) {
+  const url = req.body.url
+
+  urlShortener.short(url, function(err, shortUrl) {
+    db.Url.findOrCreate({where: {url: url, shortUrl: shortUrl}})
+    .then(([urlObj, created]) => {
+      res.send(shortUrl)
+    });
   });
 });
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
  
 
 app.listen(port, () => {
